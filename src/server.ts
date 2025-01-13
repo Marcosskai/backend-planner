1c59a6baea3b28721ba8a4fae8fbb6208af0fcc2
Marcosskai <marcos.gabrielr150@gmail.com>
2025-01-13 10:33:55 -0300

    server ok

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import fastify from "fastify";
+import { prisma } from "./lib/prisma"
 
 const app = fastify()
 
-app.get ('/test', () => {
+app.get ('/cadastrar', () => {
+    return 'Hello World'
+})
+
+app.get ('/listar', () => {
     return 'Hello World'
 })
 
